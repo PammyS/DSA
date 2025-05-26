cbad4f34b65d13bf8f8a2bb69c0f017a114462a0
Perminder Singh <perminder.singh.2105@gmail.com>
2025-05-26 14:26:21 +0530

    adding few things

--- src/Main.java
+++ src/Main.java
@@ -60,6 +60,9 @@ public class Main {
                 result[i] = stack.peek();
             }
         }
+
+
+
         result[arr.length-1] = max > arr[arr.length-1]?max:-1;
         return result;
     }
